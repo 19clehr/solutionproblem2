3fcca492eaab9a7f3d4ca108c3c9689ee3c9b066
19clehr <98840020+19clehr@users.noreply.github.com>
2023-04-14 14:48:33 -0400

    Update Solution.py

--- Solution.py
+++ Solution.py
@@ -19,6 +19,7 @@ class Solution:
         def d(self,nodeList,parentdict):
             min = -1
             min_node = -1
+            min_parent = -1
             for node in nodeList:
                 parent = parentdict(node)
                 parent_band = self.info["bandwidths"][parent]
@@ -26,8 +27,9 @@ class Solution:
                 if node_band+parent_band > min:
                     min_node = node
                     min = node_band+parent_band
+                    min_parent = parent
 
-            return min_node, min
+            return min_node, min, min_parent
         """
         This method must be filled in by you. You may add other methods and subclasses as you see fit,
         but they must remain within the Solution class.
@@ -44,17 +46,25 @@ class Solution:
               parentdict[i] = self.isp
               #node, node badwidth
         for i in self.info["list_clients"]:
-             explored = [self.isp]
-             parentdict = initparentdict
-             #explored = node,
-             currenttovisit = tovisit
-             while i not in explored:
-                    node_picked, bandpicked = d(currenttovisit,parentdict)
-                    explored.append[node_picked]
+            explored = [self.isp,-1]
+            
+            parentdict = initparentdict
+            #explored = node, parent
+            currenttovisit = tovisit
+            while i not in explored:
+                    node_picked, bandpicked, parent = d(currenttovisit,parentdict)
+                    explored.append[node_picked,parent]
+                    for j in self.graph[node_picked]:
+                         if j not in tovisit:
+                              tovisit.append(j)
+                              parentdict[j] = node_picked
                     self.info["bandwidths"][node_picked] = bandpicked
 
-
-
+            paths[i] = [i]
+            current = explored[-1][1]
+            while current != self.isp:
+                 paths[i].append(current)
+                 current = parentdict[current]
         #paths = bfs_path(self.graph,self.isp,self.info["list_clients"])
         return (paths, bandwidths, priorities)
 
